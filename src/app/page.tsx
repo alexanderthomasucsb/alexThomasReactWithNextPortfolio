a04ec55491109f6ada52c9c49edba7f90070a6a0
Alexander <alexanderthomas@ucsb.edu>
2023-08-27 19:17:43 -0700

    8/27/23

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import Image from "next/image"
 import mePhoto from 'public/AIPhoto.jpg'
 import pyPhoto from 'public/py.png'
 import githubPhoto from 'public/githubPhoto.png'
+import nba2016 from 'public/PCAofPositionsNBA2016.png'
 
 
 export default function Home() {
@@ -36,7 +37,7 @@ export default function Home() {
 
             </ul>
             <ul className=" flex items-center">
-              <li><a className= " bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href='https://drive.google.com/file/d/1V395_TK3QNNSb1mlQ7oWf0KXeRA3qu5b/view?usp=sharing' target="_blank">Resume</a></li>
+              <li><a className= " bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href='https://drive.google.com/file/d/10oUdRbsYOYsHaMuqG356VpI87hgujUKa/view?usp=drive_link' target="_blank">Resume</a></li>
             </ul>
           </nav>
           <div className=" text-center p-10 shadow-blue rounded-lg">
@@ -82,45 +83,66 @@ export default function Home() {
         </section>
 
         <section>
-          <div>
-            <div className="flex flex-col items-center gap-16 py-3">
-              <div className="text-center shadow-orange shadow-lg p-10  my-10  flex-1">
-                <div className = 'p-10'>
-                  <h3 className="text-3xl mb-10">
-                    Chess Puzzle Generator
-                  </h3>
-                    <iframe
-                      width="639"
-                      height="360"
-                      src={'https://www.youtube.com/embed/T_VrPDMSrYM'}
-                      title="YouTube video player"
-                      frameBorder="0"
-                      allowFullScreen
-                  ></iframe>
+          <div className="flex flex-col items-center gap-16 py-3">
+            <div className="text-center shadow-orange shadow-lg p-10  my-5  flex-1 max-w-xl">
+              <div className = 'p-10'>
+                <h3 className="text-3xl mb-10">
+                  Chess Puzzle Generator
+                </h3>
+                  <iframe
+                    width="420"
+                    height="260"
+                    src={'https://www.youtube.com/embed/T_VrPDMSrYM'}
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allowFullScreen
+                ></iframe>
+              </div>
+              <div className="flex-col h-40 w-639 items-center justify-center border-white p-2">
+                <div className="flex items-center justify-center">
+                  <p className="px-1">Made using with Python using Tkinter with PIL</p>
+                  <Image src={pyPhoto} alt="no image to show" width="50" height="50"/>
                 </div>
-                <div className="flex-col h-40 w-639 items-center justify-center border-white p-2">
-                  
-                  <div className="flex items-center justify-center">
-                    <p className="px-1">Made with Python using Tkinter with PIL</p>
-                    <Image src={pyPhoto} alt="no image to show" width="50" height="50"/>
-                  </div>
-                  <div className="flex items-center justify-center">
-                    <p className="px-5">Link to project: </p>
-                    <a href="https://github.com/alexanderthomasucsb/chessPuzzle-alexthomas" target="_blank" rel="noopener noreferrer" className="flex">
-                      <Image src={githubPhoto} alt="no image to show" width="50" height="50" />
-                    </a>
-                  </div>
-                 
+                <div className="flex items-center justify-center">
+                  <p className="px-5">Link to project: </p>
+                  <a href="https://github.com/alexanderthomasucsb/chessPuzzle-alexthomas" target="_blank" rel="noopener noreferrer" className="flex">
+                    <Image src={githubPhoto} alt="no image to show" width="50" height="50" />
+                  </a>
                 </div>
+              </div>
+            </div>
+
+            <div  className='text-center shadow-orange shadow-lg p-10  my-5  flex-1 max-w-xl'>
+              <div className = 'p-10'>
+                <h3 className="text-3xl mb-10">
+                  Visualization of Variation by Position in the NBA
+                </h3>
+                <p>
+                  Using principal component analysis I plotted the variation of stats: such as 3 point percentage, 2 point percentage, etc. to show how different positions in the nba vary in play style
+                </p>
+                <Image src={nba2016} alt="no image to show" width="639" height="360" />
+              </div>
+              <div className="flex-col h-40 w-639 items-center justify-center border-white p-2">
+                <div className="flex items-center justify-center">
+                  <p className="px-1">Made with Python using matplotlib and numpy</p>
+                  <Image src={pyPhoto} alt="no image to show" width="50" height="50"/>
+                </div>
+                <div className="flex items-center justify-center">
+                  <p className="px-5">Link to project: </p>
+                  <a href="https://github.com/alexanderthomasucsb/PCAanalysisNbaPositionStats/tree/main" target="_blank" rel="noopener noreferrer" className="flex">
+                    <Image src={githubPhoto} alt="no image to show" width="50" height="50" />
+                  </a>
+                </div>
+              </div>
+            </div>
+                
+          </div>
+
+          
                
                   
                   
-            </div>
-                <p>
-                  Project 2 
-                </p>
-            </div>
-          </div>
+        
         </section>
 
       </main>
